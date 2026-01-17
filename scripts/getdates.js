426dedd3a95aadebe5c5247478b227b2532a26c3
javierzunigam <jzm2006@gmail.com>
2026-01-17 17:51:09 -0600

    ww1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 // Get and display the last modified date
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
